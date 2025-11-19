9f12bf5a15f55cf244785ab431e6ddf752c027af
bavykin <bavykin@protei-lab.ru>
2025-11-19 13:06:17 +0300

    Refactoring

--- algorithms_and_data_structures/data_structures/linkedlist.py
+++ algorithms_and_data_structures/data_structures/linkedlist.py
@@ -1,78 +1,77 @@
 
 class Node:
-    def __init__(self, value):
-        self.next_node = None
-        self.previous_node = None
-        self.value = value
+    def __init__(self, data):
+        self.next = None
+        self.previous = None
+        self.data = data
 
 class DoublyLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.length = 0
-        self.iter_position = 0
+        self._head = None
+        self._tail = None
+        self._length = 0
+        self._iter_position = 0
 
 
-    def append(self, value):
-        new_node = Node(value)
-        new_node.previous_node = self.tail
-        self.tail = new_node
-        if self.head is None:
-            self.head = new_node
+    def append(self, data):
+        new_node = Node(data)
+        new_node.previous = self._tail
+        self._tail = new_node
+        if self._head is None:
+            self._head = new_node
         else:
-            current_node = self.head
-            while current_node.next_node:
-                current_node = current_node.next_node
-            current_node.next_node = new_node
-        self.length += 1
+            current_node = self._head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+        self._length += 1
         return self
 
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.head.next_node = None
+    def prepend(self, data):
+        new_node = Node(data)
+        if self._head is None:
+            self._head = new_node
+            self._head.next = None
         else:
-            new_node.next_node = self.head
-            self.head = new_node
-            self.head.next_node.previous_node = new_node
-            self.length += 1
+            new_node.next = self._head
+            self._head = new_node
+            self._head.next.previous = new_node
+            self._length += 1
         return self
 
-    def delete(self, value):
-        current_node = self.head
-        while current_node.next_node:
-            if current_node.value == value:
+    def delete(self, data):
+        current_node = self._head
+        while current_node.next:
+            if current_node.data == data:
                 break
-            current_node = current_node.next_node
-        current_node.previous_node.next_node = current_node.next_node
-        current_node.next_node.previous_node = current_node.previous_node
+            current_node = current_node.next
+        current_node.previous.next = current_node.next
+        current_node.next.previous = current_node.previous
         del current_node
-        self.length -= 1
+        self._length -= 1
         return self
 
     def __reversed__(self):
-        current_node = self.tail
+        current_node = self._tail
         while current_node:
-            current_node.next_node, current_node.previous_node = current_node.previous_node, current_node.next_node
-            current_node = current_node.next_node
-        self.head, self.tail = self.tail, self.head
+            current_node.next, current_node.previous = current_node.previous, current_node.next
+            current_node = current_node.next
+        self._head, self._tail = self._tail, self._head
         return self
 
-
     def __len__(self):
-        return self.length
+        return self._length
 
     def __iter__(self):
-        self.iter_position = 0
+        self._iter_position = 0
         return self
 
     def __next__(self):
-        if self.iter_position == self.length:
+        if self._iter_position == self._length:
             raise StopIteration
         else:
-            current_node = self.head
-            for _ in range(self.iter_position):
-                current_node = current_node.next_node
-            self.iter_position += 1
-            return current_node.value
+            current_node = self._head
+            for _ in range(self._iter_position):
+                current_node = current_node.next
+            self._iter_position += 1
+            return current_node.data
--- tests/test_doubly_linked_list.py
+++ tests/test_doubly_linked_list.py
@@ -12,29 +12,18 @@ def test_append(linked_list):
     for value in values:
         linked_list.append(value)
     assert len(linked_list) == len(values)
-
-    current_node = linked_list.head
-    for value in values:
-        assert current_node.value == value
-        current_node = current_node.next_node
-
+    assert list(linked_list) == values
 
 def test_reversed(linked_list):
     values = [0, 1, 2, 3, 4, 5]
     for value in values:
         linked_list.append(value)
-
-    reversed(linked_list)
-    current_node = linked_list.head
-    for value in reversed(values):
-        assert current_node.value == value
-        current_node = current_node.next_node
+    assert list(reversed(linked_list)) == list(reversed(values))
 
 
 def test_iteration(linked_list):
     values = [0, 1, 2, 3, 4, 5]
     for value in values:
         linked_list.append(value)
-    assert list(linked_list) == values
     for linked_value, list_value in zip(linked_list, values):
         assert linked_value == list_value
